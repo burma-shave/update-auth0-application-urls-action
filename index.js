a0f09f27cf295b9294ef6fe1a24682babd18d982
Eric Jutrzenka <e.jutrzenka@icloud.com>
2022-08-15 14:53:54 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const ManagementClient = require("auth0").ManagementClient;
+
+const ADD_CMD = "add";
+const REMOVE_CMD = "remove";
+
+const command = core.getInput("command");
+const managementClientDomain = core.getInput("management-client-domain");
+const managementClientId = core.getInput("management-client-clientId");
+const managementClientSecret = core.getInput("management-client-secret");
+
+const callbackUrl = core.getInput("callback-url");
+const logoutUrl = core.getInput("logout-url");
+
+const managementClient = new ManagementClient({
+  domain: managementClientDomain,
+  clientId: managementClientId,
+  clientSecret: managementClientSecret,
+  scope: "read:clients update:clients",
+});
+
+const apiParams = {
+  client_id: core.getInput("applicaton-id"),
+};
+
+managementClient
+  .getClient(apiParams)
+  .then((application) => updateApplication(command, application))
+  .then((updates) => managementClient.updateClient(apiParams, updates))
+  .catch((err) => core.setFailed(err.message));
+
+const updateApplication = (command, application) => {
+  console.log("before:");
+  console.log(application);
+
+  const newCallbacks = application.callbacks ? [...application.callbacks] : [];
+  const newLogoutUrls = application.allowed_logout_urls
+    ? [...application.allowed_logout_urls]
+    : [];
+
+  const callbackUrlIndex = newCallbacks.indexOf(callbackUrl);
+  const logoutUrlIndex = newLogoutUrls.indexOf(logoutUrl);
+
+  switch (command) {
+    case ADD_CMD:
+      if (callbackUrlIndex < 0) {
+        console.log(`Adding ${callbackUrl} to ${application.name} callbacks`);
+        newCallbacks.push(callbackUrl);
+      } else {
+        console.log(
+          `${callbackUrl} already exists in ${application.name} callbacks. No change needed.`
+        );
+      }
+      if (logoutUrlIndex < 0) {
+        console.log(`Adding ${logoutUrl} to ${application.name} logout urls`);
+        newLogoutUrls.push(logoutUrl);
+      } else {
+        console.log(
+          `${logoutUrl} already exists in ${application.name} logout urls. No change needed.`
+        );
+      }
+      break;
+    case REMOVE_CMD:
+      if (callbackUrlIndex < 0) {
+        console.log(
+          `${callbackUrl} does note exist in ${application.name} callbacks. No change needed.`
+        );
+      } else {
+        console.log(`Removing ${callbackUrl} to ${application.name} callbacks`);
+        newCallbacks.splice(callbackUrlIndex, 1);
+      }
+      if (logoutUrlIndex < 0) {
+        console.log(
+          `${logoutUrl} does not exist in ${application.name} logout urls. No change needed.`
+        );
+      } else {
+        console.log(`Removing ${logoutUrl} to ${application.name} logout urls`);
+        newLogoutUrls.splice(logoutUrlIndex, 1);
+      }
+      break;
+    default:
+      throw `value of command must be ${ADD_CMD} or ${REMOVE_CMD}`;
+  }
+
+
+  const updates = {
+    callbacks: newCallbacks,
+    allowed_logout_urls: newLogoutUrls,
+  };
+  
+  console.log("After:");
+  console.log({...application, ...updates});
+
+  return updates;
+};
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Update Auth0 Application URIs action
+
+This action updates the following Auth0 application URIs:
+* Callback URLs
+* Allowed Logout URLs
+
+In order to use this action you will need to have a Machine-to-Machine
+client authorised for the Auth0 Management API configured in your tennant.
+
+The client will need the following permissions:
+* `read:clients`
+* `update:clients`
+
+## Inputs
+
+## `management-client-domain`
+
+**Required** The domain of the management client.
+
+## `management-client-clientId`
+
+**Required** The client id of the management client.
+
+## `management-client-secret`
+
+**Required** The management client secret.
+
+## `applicaton-id`
+
+**Required** The application we are updating.
+
+## `command`
+
+**Required** How to update the list, can be `add` or `remove`.
+
+## `callback-url`
+
+**Required** Allowed callback URL.
+
+## `logout-url`
+
+**Required** Allowed logout URL.
+
+## Example usage
+
+uses: burma-shave/update-auth0-application-urls-action@v0.1 
+with:
+  management-client-domain: your-tenant-name.auth0.com
+  management-client-clientId: RHYQEGP0DQxQ9hjdiaY7EX0Y5nIGJexYT
+  management-client-secret: ${{ secrets.AUTH0_MANAGEMENT_CLIENT_SECRET }} 
+  applicaton-id: rZVq9pOfki9djt7i9piGpRGgcOf
+  command: add
+  callback-url: https://foo.com/callback
+  logout-url: https://foo.com/logout
